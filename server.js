2b0e43b37e43e3d4230c6509388c29b94957185d
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-10-07 18:32:40 +1030

    package.json and server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+//require express and mongoose 
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`************** Connected on localhost:${PORT} ************* `);
+    });
+})
