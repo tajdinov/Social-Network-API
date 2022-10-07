b0bc35b92d3e0e0c28a6048aa421b80741f83f13
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-10-07 18:42:49 +1030

    Create connection.js

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const { connect, connection } = require('mongoose');
+
+
+const connectionString =
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+module.exports = connection;
